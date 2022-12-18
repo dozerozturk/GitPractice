23758239ada746b3221523831bd32b589319c93d
dozerozturk <ozturkozer@yahoo.com>
2022-12-18 07:48:43 -0600

    update

--- src/pac1/Test6.java
+++ src/pac1/Test6.java
@@ -2,6 +2,10 @@ package pac1;
 
 public class Test6 {
 
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
     //note
     //note2
     //note3
@@ -12,4 +16,5 @@ public class Test6 {
     //feedback5
     //note6
     //feedback6
+    //note7
 }
